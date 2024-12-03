fb5af121d259fa1e84df6a75a0ef969136331e8d
Stuart Forrester <stforrester@gmail.com>
2024-12-03 03:13:36 -0600

    adds designated content to Home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const homeStyle = {
+  color: "firebrick"
+};
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
-}
+  return (
+  <div>Home
+    <div id="home">
+      <h1 style={homeStyle}>{name} is a Web Developer from {city}</h1>
+    </div>
+  </div>
+  )
+};
 
 export default Home;
