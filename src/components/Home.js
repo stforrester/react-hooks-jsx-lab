3bfc339fc2d300989215b4d7a4209013304cd628
Stuart Forrester <stforrester@gmail.com>
2024-12-03 03:17:53 -0600

    adds designated content to About.js component and removes extraneous starting text from other components

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const homeStyle = {
 function Home() {
   // update the JSX being returned!
   return (
-  <div>Home
+  <div>
     <div id="home">
       <h1 style={homeStyle}>{name} is a Web Developer from {city}</h1>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   // update the JSX being returned!
   return (
-  <nav>NavBar
+  <nav>
     <a href="#home">Home</a>
     <a href="#about">About</a>
   </nav>
